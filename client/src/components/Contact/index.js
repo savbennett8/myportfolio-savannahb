617d408912b2cb4f8f01bfddf56fa9cbd22d5b72
Savannah Bennett <savvy.bennett8@gmail.com>
2021-07-12 12:37:20 -0600

    contactForm func. to handle change and submit

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -4,21 +4,31 @@ function ContactForm() {
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
     const { name, email, message } = formState;
 
+    function handleChange(e) {
+        //sync state with user input from the DOM
+        setFormState({ ...formState, [e.target.name]: e.target.value })
+    };
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    };
+
     return (
         <section>
             <h1>Contact Info</h1>
-            <form id="contact-form">
+            <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Your Name:</label>
-                    <input type="text" name="name" defaultValue={name} />
+                    <input type="text" name="name" defaultValue={name} onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="email">Your Email:</label>
-                    <input type="email" name="email" defaultValue={email} />
+                    <input type="email" name="email" defaultValue={email} onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" defaultValue={message} />
+                    <textarea name="message" rows="5" defaultValue={message} onChange={handleChange} />
                 </div>
             </form>
         </section>
