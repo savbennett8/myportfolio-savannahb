561e86876ada5a14a91238bd85e4ec74d825aee0
Savannah Bennett <savvy.bennett8@gmail.com>
2021-07-12 20:53:23 -0600

    added header

--- client/src/components/Intro/index.js
+++ client/src/components/Intro/index.js
@@ -2,13 +2,18 @@ import React from ("react");
 
 function Intro() {
     return (
-        <div id="intro">
-            <h1>Savannah Bennett</h1>
-            <p>Welcome to my Full-Stack Web Development Portfolio. Come on in, I hope you see something you like.</p>
-            <ul class="actions">
-                <li><a href="#header" class="button icon solid solo fa-arrow-down scrolly">Continue</a></li>
-            </ul>
-        </div>
+        <>
+            <div id="intro">
+                <h1>Savannah Bennett</h1>
+                <p>Welcome to my Full-Stack Web Development Portfolio. Come on in, I hope you see something you like.</p>
+                <ul class="actions">
+                    <li><a href="#header" class="button icon solid solo fa-arrow-down scrolly">Continue</a></li>
+                </ul>
+            </div>
+            <header id="header">
+                <a href="/" class="logo">Savannah</a>
+            </header>
+        </>
     );
 };
 
