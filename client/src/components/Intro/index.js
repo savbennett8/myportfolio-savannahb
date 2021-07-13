b5247f7313fe0900cd402be2c29ef6bc84b97126
Savannah Bennett <savvy.bennett8@gmail.com>
2021-07-13 13:46:22 -0600

    trying to fix scroll styling in intro - not working yet

--- client/src/components/Intro/index.js
+++ client/src/components/Intro/index.js
@@ -7,7 +7,7 @@ function Intro() {
                 <h1>Savannah Bennett</h1>
                 <p>Welcome to my Full-Stack Web Development Portfolio. Come on in, I hope you see something you like.</p>
                 <ul className="actions">
-                    <li><a href="#about" className="button icon solid solo fa-arrow-down scrolly">Continue</a></li>
+                    <li><a href="#about" className="button icon solid solo scrolly"><i class="icon-arrow-down"></i></a></li>
                 </ul>
             </div>
             <header id="header">
