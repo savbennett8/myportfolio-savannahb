5c9fa82650bf7947a3277e7df69729a89993dde0
Savannah Bennett <savvy.bennett8@gmail.com>
2021-07-12 20:51:35 -0600

    modify intro

--- client/src/components/Intro/index.js
+++ client/src/components/Intro/index.js
@@ -4,6 +4,7 @@ function Intro() {
     return (
         <div id="intro">
             <h1>Savannah Bennett</h1>
+            <p>Welcome to my Full-Stack Web Development Portfolio. Come on in, I hope you see something you like.</p>
             <ul class="actions">
                 <li><a href="#header" class="button icon solid solo fa-arrow-down scrolly">Continue</a></li>
             </ul>
