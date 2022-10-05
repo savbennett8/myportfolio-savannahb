2112c16165493c63f2eb9f877e5504c2e1103aa6
savbennett8 <savvy.bennett8@gmail.com>
2022-10-05 08:14:01 -0600

    tryig to fix resumeModal

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div id="wrapper" className="fade-in">
       <Intro></Intro>
-      <Nav
+      <Nav id="main"
         contactSelected={contactSelected}
         setContactSelected={setContactSelected}
       ></Nav>
